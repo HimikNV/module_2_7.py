347f1a36732cb3ef96684ac956525b4d5bee1d42
HimikNV <aasokotov@mail.ru>
2024-06-18 22:26:00 +0300

    Add files via upload

---
+++ Дз 2.7.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b,c)
+
+
+print_params()
+print_params(2,  3)
+print_params("", "", " ")
+print_params(a=" ", c=False)
+print_params(b=25, c=[1, 2, 3])
+values_list = [22, "Ok", False]
+values_dict = {"a": 5, "b": True, "c": "not Ok"}
+print_params(*values_list)
+print_params(**values_dict)
+values_list2 = [6, 3.5]
+print_params(*values_list2, 42)
